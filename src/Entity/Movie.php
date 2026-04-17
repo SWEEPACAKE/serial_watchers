7e8e6d77c76e3d3ad5c9c31393f2fa9e5e4e953c
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-04-17 08:49:40 +0200

    Correction TP Serial Watchers 2

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -2,40 +2,63 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
 use App\Repository\MovieRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get()
+    ],
+    normalizationContext: ['groups' => ['movie:read']]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    "genre" => "exact",
+    "director" => "exact"
+])]
 class Movie
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['movie:read'])]
     private ?int $id = null;
 
     #[Assert\NotBlank(message: "Le titre est obligatoire")]
     #[Assert\Length(max: 150, maxMessage: "Le titre ne peut pas faire plus de 150 caractères")]
     #[ORM\Column(length: 150)]
+    #[Groups(['movie:read', 'director:read'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['movie:read'])]
     private ?string $synopsis = null;
 
     #[Assert\NotBlank(message: "L'année de sortie est obligatoire")]
     #[Assert\GreaterThan(1888, message: "Petit malin, aucun film n'est aussi vieux :)")]
     #[Assert\LessThanOrEqual(value: 2026, message: "On n'est pas dans le futur !")]
     #[ORM\Column]
+    #[Groups(['movie:read', 'director:read'])]
     private ?int $releaseYear = null;
 
     #[Assert\NotBlank(message: "Le genre est obligatoire")]
     #[ORM\Column(length: 50)]
+    #[Groups(['movie:read'])]
     private ?string $genre = null;
 
     #[Assert\NotNull(message: "Le réalisateur est obligatoire")]
     #[ORM\ManyToOne(inversedBy: 'movies')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['movie:read'])]
     private ?Director $director = null;
 
     public function getId(): ?int
